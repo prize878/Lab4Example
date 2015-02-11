d2931c48ef5f304bc2be47a0af922a65858963f3
student <student@iMac-2.local>
2015-02-11 12:07:28 +0700

    Example

--- app/src/main/java/th/ac/tu/siit/its333/lab4example/MainActivity.java
+++ app/src/main/java/th/ac/tu/siit/its333/lab4example/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends ActionBarActivity
         SQLiteDatabase dbw = helper.getWritableDatabase();
         ContentValues ri = new ContentValues();
         ri.put("title", "AI homework");
-        ri.put("priority", 3);
+        ri.put("priority", 4);
         long new_id = dbw.insert("todo", null, ri);
         dbw.close();
 
